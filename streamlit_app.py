e65b7f5bdccb7d845609c8fbfecbe5ce73acfe10
JackKelly000 <110823489+JackKelly000@users.noreply.github.com>
2024-08-15 20:45:18 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,8 @@ st.title('NBAPred')
 
 st.info('Machine Learning For NBA Prediction!')
 
-# Read in the data
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-df
+with st.expander('Data'):
+  st.write('**Raw Data**')
+  # Read in the data
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df # Display df in the app
