78f73de600814904d844bda6863f5bd0c946c40f
JackKelly000 <110823489+JackKelly000@users.noreply.github.com>
2024-08-15 20:42:41 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title('NBAPred')
 
 st.info('Machine Learning For NBA Prediction!')
+
+# Read in the data
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+df
