7d370f0e5775ea4d4a630395baac09c387d7b9fd
JackKelly000 <110823489+JackKelly000@users.noreply.github.com>
2024-08-15 20:57:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,4 +22,4 @@ with st.expander('Data'):
   Y
 
 with st.expander('Data Visualization'):
-  st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='island')
+  st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
